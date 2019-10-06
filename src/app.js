bd70a047c0dea1b51fe82a1e5496b55612250764
Ryan Wiggins <ryanwiggins52@gmail.com>
2019-10-05 20:22:39 -0400

    git and display posts functionality added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import { http } from './http'
+import { ui } from './ui'
+// Get post on DOM load
+document.addEventListener('DOMContentLoaded', getPosts)
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
-
-getData('https://jsonplaceholder.typicode.com/posts');
+function getPosts () {
+  http
+    .get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err))
+}
