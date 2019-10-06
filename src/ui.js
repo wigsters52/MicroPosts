e4412002b9e71747da5f3cd11d7413b965a400a2
Ryan Wiggins <ryanwiggins52@gmail.com>
2019-10-06 11:51:02 -0400

    update tool added

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,5 @@
 class UI {
-  constructor() {
+  constructor () {
     this.post = document.querySelector('#posts')
     this.titleInput = document.querySelector('#title')
     this.bodyInput = document.querySelector('#body')
@@ -8,31 +8,33 @@ class UI {
     this.forState = 'add'
   }
 
-  showPosts(posts) {
+  // Show all posts
+  showPosts (posts) {
     let output = ''
 
     posts.forEach(post => {
       output += `
-          <div class="card mb-3">
-            <div class="card-body">
-              <h4 class="card-title">${post.title}</h4>
-              <p class="card-text">${post.body}</p>
-              <a href="#" class="edit card-link" data-id="${post.id}">
-                <i class="fa fa-pencil"></i>
-              </a>
-  
-              <a href="#" class="delete card-link" data-id="${post.id}">
-              <i class="fa fa-remove"></i>
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="card-title">${post.title}</h4>
+            <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+              <i class="fa fa-pencil"></i>
             </a>
-            </div>
+
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
+          </a>
           </div>
-        `
+        </div>
+      `
     })
 
     this.post.innerHTML = output
   }
 
-  showAlert(message, className) {
+  // Show alert message
+  showAlert (message, className) {
     this.clearAlert()
 
     // Create div
@@ -54,7 +56,8 @@ class UI {
     }, 3000)
   }
 
-  clearAlert() {
+  // Clear alert message
+  clearAlert () {
     const currentAlert = document.querySelector('.alert')
 
     if (currentAlert) {
@@ -62,9 +65,55 @@ class UI {
     }
   }
 
-  clearFields() {
+  // Clear all fields
+  clearFields () {
     this.titleInput.value = ''
     this.bodyInput.value = ''
+  }
+
+  // Fill form to edit
+  fillForm (data) {
+    this.titleInput.value = data.title
+    this.bodyInput.value = data.body
+    this.idInput.value = data.id
+
+    this.changeFormState('edit')
+  }
+
+  // Clear ID hidden value
+  clearIdInput () {
+    this.idInput.value = ''
+  }
+
+  // Change the form state
+  changeFormState (type) {
+    if (type === 'edit') {
+      this.postSubmit.textContent = 'Update Post'
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block'
+
+      // Create cancel button
+      const button = document.createElement('button')
+      button.className = 'post-cancel btn btn-light btn-block'
+      button.appendChild(document.createTextNode('Cancel Edit'))
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form')
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end')
+      // Insert cancel button
+      cardForm.insertBefore(button, formEnd)
+    } else {
+      this.postSubmit.textContent = 'Post It'
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block'
+      // Remove cancel btn if it is there
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove()
+      }
+      // Clear ID from hidden field
+      this.clearIdInput()
+      // Clear text
+      this.clearFields()
+    }
   }
 }
 
